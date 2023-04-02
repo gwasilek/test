410740fd75f77a970a36942168dc378abde43edc
Grzegorz Wasiłek <65309116+gwasilek@users.noreply.github.com>
2023-04-02 23:11:25 +0200

    Update and rename gaps2.js to gaps1.js

---
+++ gaps1.js
@@ -0,0 +1,15 @@
+var tl = gsap.timeline({ repeat:4, yoyo:true, defaults:{opacity:0, ease:"back"}});
+gsap.set(".rl-page-wrapper", {rotationY:15})
+function init() {
+    tl.from("rl-page-wrapper", {ease:"linear", autoAlpha:0})
+    .from(".heading-style-h1.is-one", {x:80, duration:1})
+    .from(".heading-style-h1.is-two", {x:-80, duration:1}, "<")
+    .from(".text-size-regular", {y:30}, "-=0.2")
+    .from(".button-group", {y:50}, "-=0.4") 
+    .from(".cursor_dot", {scale:0, transformOrigin:"50% 50%", stagger:0.1}, "-=0.5")
+}
+
+window.addEventListener("load", function(event) { 
+  init(); //do stuff
+  GSDevTools.create({animation:tl})
+});
