ab8049806e7c70282079ebf7398c93b3026c2080
Grzegorz Wasiłek <65309116+gwasilek@users.noreply.github.com>
2023-04-02 23:21:08 +0200

    Update and rename gaps3.js to gaps4.js

--- gaps4.js
+++ gaps4.js
@@ -1,4 +1,4 @@
-var tl = gsap.timeline({ repeat:0, defaults:{opacity:0, ease:"back"}});
+var tl = gsap.timeline({defaults:{opacity:0, ease:"back"}});
 gsap.set(".rl-page-wrapper", {rotationY:15})
 function init() {
     tl.from("rl-page-wrapper", {ease:"linear", autoAlpha:0})
@@ -6,7 +6,7 @@ function init() {
     .from(".heading-style-h1.is-two", {x:-80, duration:1}, "<")
     .from(".text-size-regular", {y:30}, "-=0.2")
     .from(".button-group", {y:50}, "-=0.4") 
-    .from(".cursor_dot", {scale:0, transformOrigin:"50% 50%", stagger:0.1}, "-=0.5")
+    .from(".cursor_dot", {scale:0, transformOrigin:"50% 50%", duration:0.8}, "-=0.5")
 }
 
 window.addEventListener("load", function(event) { 
