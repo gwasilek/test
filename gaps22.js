9242123d024020168ca3a9404f8156a814547236
Grzegorz Wasiłek <65309116+gwasilek@users.noreply.github.com>
2023-04-04 21:27:00 +0200

    Update and rename gaps21.js to gaps22.js

--- gaps22.js
+++ gaps22.js
@@ -6,26 +6,27 @@ function init() {
     .from(".text-size-regular", {y:30}, "-=0.2")
     .from(".button-group", {y:50}, "-=0.4")
     .from(".yerold", {scale:0, transformOrigin:"50% 50%", duration:0.8}, "-=0.4")
+    .from(".header_image", {height:"100%", width:"100%", ease:"power4.out", duration: 5.25, transformOrigin:"50% 50%"}, 0.1);
 }
 
-window.addEventListener("load", function(event) { 
-  init(); //do stuff
-});
+//window.addEventListener("load", function(event) { 
+  //init(); //do stuff
+//});
 
 
 // On Page Load
-function pageLoad() {
-  let tl = gsap.timeline({ delay: 0.1 });
-  tl.to(".header_image", {
-   height: "100%",
-    width: "100%",
-    ease: "power4.out",
-    duration: 5.25,
-    transformOrigin:"50% 50%"
-   },
-   0.1
-   );
-}
+//function pageLoad() {
+  //let tl = gsap.timeline({ delay: 0.1 });
+  //tl.to(".header_image", {
+  // height: "100%",
+    //width: "100%",
+    //ease: "power4.out",
+    //duration: 5.25,
+    //transformOrigin:"50% 50%"
+  // },
+   //0.1
+   //);
+//}
 pageLoad();
 gsap.defaults({ duration: 1.5, ease: "power2.out" });
 
