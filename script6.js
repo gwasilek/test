c685d5cf5db16645394f643b091c64c21091a683
Grzegorz Wasiłek <65309116+gwasilek@users.noreply.github.com>
2023-04-04 19:17:17 +0200

    Update script6.js

--- script6.js
+++ script6.js
@@ -6,8 +6,7 @@ function init() {
     .from(".heading-style-h1.is-two", {x:-80, duration:1}, "<")
     .from(".text-size-regular", {y:30}, "-=0.2")
     .from(".button-group", {y:50}, "-=0.4") 
-    .from(".yer", {scale:0, transformOrigin:"50% 50%", duration:0.8}, 0)
-    .from(".script5.is-title", {scale:0, transformOrigin:"50% 50%", duration:0.8}, "<")
+    .from(".script5.is-title", {scale:0, transformOrigin:"50% 50%", duration:0.8}, "-=0.4")
 
 }
 
