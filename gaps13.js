57614d9eedc8dc7bb438ed5d96e08b11d0d7a98a
Grzegorz Wasiłek <65309116+gwasilek@users.noreply.github.com>
2023-04-04 18:58:39 +0200

    Update and rename gaps12.js to gaps13.js

--- gaps13.js
+++ gaps13.js
@@ -5,7 +5,8 @@ function init() {
     .from(".heading-style-h1.is-two", {x:-80, duration:1}, "<")
     .from(".text-size-regular", {y:30}, "-=0.2")
     .from(".button-group", {y:50}, "-=0.4") 
-    .from(".cursor_dot", {scale:0, transformOrigin:"50% 50%", duration:0.8}, "-=0.5")
+    .from(".yer", {scale:0, transformOrigin:"50% 50%", duration:0.8}, "-=0.5")
+    .from(".cyrcle", {scale:0, transformOrigin:"50% 50%", duration:0.8}, "-=0.5")
 }
 
 window.addEventListener("load", function(event) { 
@@ -23,7 +24,7 @@ function pageLoad() {
     duration: 5.25,
     transformOrigin:"50% 50%"
    },
-   0.5
+   0.1
    );
 }
 pageLoad();
