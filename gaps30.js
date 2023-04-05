8cb146edcee90784330ab36ee180091a81cf630e
Grzegorz Wasiłek <65309116+gwasilek@users.noreply.github.com>
2023-04-05 20:28:13 +0200

    1

--- gaps30.js
+++ gaps30.js
@@ -44,40 +44,4 @@ function letterAnimation() {
     tl.from(".h2-text.gradient.is-1", {x:80, duration:1})
     .from(".h2-text.gradient.is-2", {x:-80, duration:1}, "<"); 
 
-do {
-  
-      letterAnimation();
-} while (condition);
-
-var pages;
-// html example: https://g6jds.csb.app/range_slider.txt
-$(document).ready(function () {
-    pages = 25;
-    $('[name=pages]').val(pages);
-    swiper_func_pages("pages", '.slider-number');
-    do_on_range_change_pages();
-  });
-
-  $("[name=pages]").on("input", function () {
-    pages = $('[name=pages]').val();
-    swiper_func_pages("pages", '.slider-number');
-    do_on_range_change_pages();
-  });
-
-  function do_on_range_change_pages() {
-      $('.slider-number').text(pages);
-  $('[bloc=budget]').text((pages * 4));
-  $('[bloc=budget]').text((pages * 4));
-
-  }
-
-  function swiper_func_pages(name, swiper) {
-    name = "[name=" + name + "]";
-    var range_val = $(name).val();
-    range_val = parseFloat(range_val);
-    var range_width = $(name).width() - 10;
-    var range_max = $(name).attr("max");
-    range_width = range_width / range_max;
-    range_width = range_width * (range_val - 1);
-    $(swiper).css({ transform: "translateX(" + range_width + "px)" });
-  }
+    letterAnimation();
