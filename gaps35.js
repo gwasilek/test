bcbd7565d0abea9b7651d86f027a322917123fd1
Grzegorz Wasiłek <65309116+gwasilek@users.noreply.github.com>
2023-04-06 13:39:53 +0200

    Update gaps35.js

--- gaps35.js
+++ gaps35.js
@@ -34,6 +34,6 @@ $(".menu_link").on("mouseleave", function () {
 ScrollTrigger.create({
 	trigger:".layout",
 	start:"bottom 90%",
-	animation:gsap.from(".col", {opacity:0, y:60}),
+	animation:gsap.from(".col", {opacity:0, y:60, stagger:0.2,}),
 	toggleActions:"restart none none reverse"
 })
